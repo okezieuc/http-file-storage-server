f8f27b78cdfb5fdec093b2d474af8c86c0319383
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-01-21 19:45:17 -0600

    add support for fetching files in static directory

--- handle_get.h
+++ handle_get.h
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int handle_get(int client_socket_fd, char *path)
 {
+    long int resource_size;
     char resource_path[1000];
     FILE *fptr = NULL;
+    void *resource;
 
     strcpy(resource_path, "static");
     strcat(resource_path, path); // we assume that the path starts with a "/"
@@ -21,9 +24,20 @@ int handle_get(int client_socket_fd, char *path)
     }
 
     // allocate enough memory to contain the file
+    fseek(fptr, 0, SEEK_END);
+    resource_size = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
 
     // send relevant headers for the file
+    send_string(client_socket_fd, "HTTP/1.0 200 OK\r\n");
+    send_string(client_socket_fd, "Server: File Storage Server\r\n\r\n");
 
     // send the contents of the file to the client
+    resource = malloc(resource_size);
+    fread((char *)resource, 1, resource_size, fptr);
+    fclose(fptr);
+    send(client_socket_fd, resource, resource_size, 0);
+
+    free(resource);
     return 0;
 }
